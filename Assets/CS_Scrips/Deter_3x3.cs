ff32873249256165f6881f1c96ef25a1a276774d
wilkintrinidad10@gmail.com <wilkintrinidad10@gmail.com>
2021-10-12 16:57:39 -0700

    Add project files.

---
+++ Assets/CS_Scrips/Deter_3x3.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Deter_3x3 : MonoBehaviour
+{
+ 
+    public int[] matriz = new int[9];
+
+    /* 
+    * [ 0, 1 ,2
+    *   3, 4 ,5
+    *   6, 7 ,8
+    *   0, 1 ,2
+    *   3, 4 ,5]
+    */
+    public void Generar_Determinante()
+    {
+        var Dia_prin_1 = matriz[0] * matriz[4] * matriz[8];
+        var Dia_prin_2 = matriz[3] * matriz[7] * matriz[2];
+        var Dia_prin_3 = matriz[6] * matriz[1] * matriz[5];
+
+        var Dia_Secun_1 = matriz[2] * matriz[4] * matriz[6];
+        var Dia_Secun_2 = matriz[5] * matriz[7] * matriz[0];
+        var Dia_Secun_3 = matriz[8] * matriz[1] * matriz[3];
+
+        var Diagonal_principal = Dia_prin_1 + Dia_prin_2 + Dia_prin_3;
+        var Diagonal_Secundaria = Dia_Secun_1 + Dia_Secun_2 + Dia_Secun_3;
+
+        var determinante = Diagonal_principal - Diagonal_Secundaria;
+
+        Calculate_Controller._Controller.Determinante(determinante);
+        
+    }
+}
---
+++ Assets/CS_Scrips/agrega_numero.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class agrega_numero : MonoBehaviour
+{
+    [SerializeField]
+    private int numero;
+
+    public void Agrega()
+    {
+        Calculate_Controller._Controller.Uptade_Numbers(numero);
+        Calculate_Controller._Controller.aumentar_Rango();
+        print("se agrego el numero" + numero);
+    }
+
+}
